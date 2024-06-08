b1de04868ae8ee8605df61f73fa45ddfb6f2ad22
JosePu99 <joseluis-pusam@cunoc.edu.gt>
2024-06-07 22:59:54 -0600

    Cambios para agregar numero de lineas, cambios en textareas, en index y en css, script para hacer numeros dinamicos

--- scripts.js
+++ scripts.js
@@ -1,34 +1,47 @@
 fetch('html/navbar.html')
-        .then(response => response.text())
-        .then(data => document.getElementById('navbar').innerHTML = data);
-       
-        fetch('html/textareas.html')
-        .then(response => response.text())
-        .then(data => document.getElementById('textareas').innerHTML = data);
-        
-        fetch('html/tabs.html')
-        .then(response => response.text())
-        .then(data => document.getElementById('tabs').innerHTML = data);
+    .then(response => response.text())
+    .then(data => document.getElementById('navbar').innerHTML = data);
 
-        function saludo(){
-            alert("hola Douglas");
-        }
-        function optimizar(){
-            alert("Optimizado con exito xd");
-        }
-        function CargarArchivo(){
-            const fileInput = document.getElementById('fileInput');
-            const textarea = document.getElementById('textoEntrada');
+fetch('html/textareas.html')
+    .then(response => response.text())
+    .then(data => {
+        document.getElementById('textareas').innerHTML = data;
+        actualizarLineas(); // Inicializar los números de línea
+    });
 
-            const file = fileInput.files[0];
-            if (file) {
-                const reader = new FileReader();
+fetch('html/tabs.html')
+    .then(response => response.text())
+    .then(data => document.getElementById('tabs').innerHTML = data);
 
-                reader.onload = function(e) {
-                    textarea.value = e.target.result;
-                };
+function saludo() {
+    alert("hola Douglas");
+}
 
-                reader.readAsText(file);
-                alert("Archivo cargado con exito");
-            }
-        }
+function optimizar() {
+    alert("Optimizado con exito xd");
+}
+
+function CargarArchivo() {
+    const fileInput = document.getElementById('fileInput');
+    const textarea = document.getElementById('textoEntrada');
+
+    const file = fileInput.files[0];
+    if (file) {
+        const reader = new FileReader();
+
+        reader.onload = function(e) {
+            textarea.value = e.target.result;
+            actualizarLineas();
+        };
+
+        reader.readAsText(file);
+        alert("Archivo cargado con exito");
+    }
+}
+
+function actualizarLineas() {
+    const textarea = document.getElementById('textoEntrada');
+    const lineNumbers = document.getElementById('lineNumbers');
+    const lines = textarea.value.split('\n').length;
+    lineNumbers.innerHTML = Array.from({ length: lines }, (_, i) => i + 1).join('<br>');
+}
